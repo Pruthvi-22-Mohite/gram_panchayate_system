49d1b9d3330178d98499050e9b248236f8903472
Vivek mhantati <vmhantati@gmail.com>
2025-11-16 11:59:03 +0530

    Initial

--- gram_panchayate_system/settings.py
+++ gram_panchayate_system/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gram_panchayate_system',
         'USER': 'root',
-        'PASSWORD': 'Pruthvi@22',  # Leave empty if no password, or add your password here
+        'PASSWORD': 'Vivek@M2007',  # Leave empty if no password, or add your password here
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
